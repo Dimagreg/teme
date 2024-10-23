78943923d53e3b179783195ad4d37c276b702c37
Dmitrii Grigori <dmitrii.grigori@opswat.com>
2024-10-23 17:45:57 +0300

    update APD

--- an3/sem1/APD/assigment1/epidemics_simulation/main.c
+++ an3/sem1/APD/assigment1/epidemics_simulation/main.c
@@ -4,6 +4,10 @@
 #include <errno.h>
 #include <limits.h> // for INT_MIN and INT_MAX
 
+#define INFECTED_DURATION 20 // simulation time a person is infected
+#define IMMUNE_DURATION 50 // simulation time a person is immune to being infected
+
+
 long TOTAL_SIMULATION_TIME;
 char inputFileName[256];
 long ThreadNumber;
@@ -12,23 +16,28 @@ int MAX_X_COORD;
 int MAX_Y_COORD;
 int PEOPLE_COUNT;
 
-int ZONE[][]; // simulation zone where we compute if a person gets infected or not
+int **ZONE; // 2D simulation zone where we compute if a person gets infected or not. (infected=1, safe=0)
+            // In present case we consider that person gets infected if he is in the same coord as infected one.
 
 struct person
 {
     int personId;
     int x, y; // coords 
-    int current_status; // (infected=0, susceptible = 1)
+    int current_status; // (infected=0, susceptible = 1, immune = 2)
     int next_status; // status that will define current_status on next simulation
     int pattern_direction; // (N=0, S=1, E=2, W=3)
     int pattern_amplitude; // step amplitude, doesn't get infected if steps across the infected zone in one simulation
+    int effect_time_left; // (infected or immune)
 };
 
 void readArguments(char* argv[]);
 void readInputFile(struct person **st_person);
+void initPersonEffectDuration(struct person *person);
 void processSimulationSequential(struct person **st_person);
 void updateLocationSequential(struct person **st_person);
+void computeZoneSequential(struct person *person);
 void computeNextStatusSequential(struct person **st_person);
+void computePersonNextStatus(struct person *person);
 void setCurrentStatusSequential(struct person **st_person);
 
 int main(int argc, char* argv[])
@@ -107,14 +116,30 @@ void readInputFile(struct person **st_person)
         exit(1);
     }
 
+    // Allocate size for 2D array ZONE
+    if ((ZONE = (int**) malloc(MAX_X_COORD * sizeof(int))) == NULL)
+    {
+        perror(NULL);
+        exit(1);
+    }
+
+    for (int i = 0; i < MAX_X_COORD; i++)
+    {
+        if ((ZONE[i] = (int*) malloc(MAX_Y_COORD * sizeof(int))) == NULL)
+        {
+            perror(NULL);
+            exit(1);
+        }
+    }
+
     if (fscanf(file, "%d", &PEOPLE_COUNT) != 1)
     {
         printf("error reading PEOPLE_COUNT\n");
         exit(1);
     }
 
-    if ((*st_person = malloc(PEOPLE_COUNT * sizeof(struct person))) == NULL)
-    {
+    if ((*st_person = (struct person*) malloc(PEOPLE_COUNT * sizeof(struct person))) == NULL)
+    { 
         perror(NULL);
         exit(1);
     }
@@ -127,13 +152,53 @@ void readInputFile(struct person **st_person)
             printf("error reading people. check if lines are formatted correctly.\n");
             exit(1);
         }
+
+        setZone(&(*st_person)[i]);
+
+        initPersonEffectDuration(&(*st_person)[i]);
+    }
+}
+
+void setZone(struct person *person)
+{
+    if (person->current_status == 0)
+    {
+        ZONE[person->x][person->y] = 1;
+    }
+    else
+    {
+        ZONE[person->x][person->y] = 0;
+    }
+}
+
+void initPersonEffectDuration(struct person *person)
+{
+    switch (person->current_status)
+    {
+        case 0: 
+            person->effect_time_left = INFECTED_DURATION;
+            break;
+
+        case 1: 
+            person->effect_time_left = 0;
+            break;
+
+        case 2: 
+            person->effect_time_left = IMMUNE_DURATION;
+            break;
+
+        default:
+            printf("invalid pattern direction\n");
+            exit(1);
     }
 }
 
 void processSimulationSequential(struct person **st_person)
 {
     // we consider on simulation time 0 that everyone "spawns" and no one gets infected by it's neighbour.
-    // update location, compute next status, set current status and increment simulation time.
+    // update location, 
+    // compute next status based on current position with infected person and check for effects durations
+    // set current status and increment simulation time.
 
     for (int i = 0; i < TOTAL_SIMULATION_TIME; i++)
     {
@@ -157,20 +222,27 @@ void updateLocationSequential(struct person **st_person)
 
 void computeLocation(struct person *person)
 {
+    //remove previous location from ZONE -> makes it safe
+    ZONE[person->x][person->y] = 0;
+
     switch (person->pattern_direction)
     {
     case 0: // N -> y++
         person->y += person->pattern_amplitude;
         break;
+
     case 1: // S -> y--
         person->y -= person->pattern_amplitude;
         break;
+
     case 2: // E -> x++
         person->x += person->pattern_amplitude;
         break;
+
     case 3: // W -> x--
         person->x -= person->pattern_amplitude;
         break;
+
     default:
         printf("invalid pattern direction\n");
         exit(1);
@@ -201,10 +273,47 @@ void computeLocation(struct person *person)
         person->y = MAX_Y_COORD;
         person->pattern_direction = 1;
     }
+
+    
+    // mark current zone based on next status from previous simulation time
+    setZone(person);
 }
+
 
 void computeNextStatusSequential(struct person **st_person)
 {
-    
+    for (int i = 0; i < PEOPLE_COUNT; i++)
+    {
+        computePersonNextStatus(&(*st_person)[i]);
+    }
 }
+
+void computePersonNextStatus(struct person *person)
+{
+    //compute if effects expired
+    if (person->effect_time_left == 0)
+    {
+        switch (person->current_status)
+        {
+            case 0: //infection expired -> make immune
+                person->current_status = 2;
+                person->effect_time_left = IMMUNE_DURATION;
+        }
+    }
+
+    if (ZONE[person->x][person->y] == 1)
+    {
+        // person is susceptible -> set next status to infected
+        if (person->current_status == 1)
+        {
+            person->next_status = 0;
+        }
+    }
+    else
+    {
+
+    }
+}
+
 void setCurrentStatusSequential(struct person **st_person);
+ 
